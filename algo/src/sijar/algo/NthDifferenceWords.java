63b9bb05581b5e3d6d21666c94aff28a4ee6d75e
s <28989555+sijar@users.noreply.github.com>
2019-02-06 22:58:48 +0530

    Added algorithm to find Nth difference of words

---
+++ algo/src/sijar/algo/NthDifferenceWords.java
@@ -0,0 +1,70 @@
+/*
+ * Developed by Sijar Ahmed on 6/2/19 6:52 PM
+ * Last modified 6/2/19 6:52 PM.
+ * Sijar Ahmed (sijar.ahmed@gmail.com)
+ * Copyright (c) 2019. All rights reserved.
+ *
+ *
+ * The Class / Interface NthDifferenceWords is responsible for...
+ * @author sijarahmed
+ * 6/2/19 6:52 PM
+ *
+ */
+
+package sijar.algo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NthDifferenceWords {
+
+
+    public static void main(String[] args) {
+        int nth_difference = 1;
+        String pattern = "mert";
+        String[] data = new String[]{"aert", "ert", "meat", "mmert", "merst", "merts","meritos"};
+        System.out.println(getNthDifferenceWords(pattern,data,nth_difference));
+
+        nth_difference = 2;
+        pattern = "merit";
+        data = new String[]{"aert", "ert", "meat", "mmert", "merst", "merts","demerit","merito", "meritos"};
+        System.out.println(getNthDifferenceWords(pattern,data,nth_difference));
+    }
+
+    /**
+     * Then function will return list of the words matched with nth_difference
+     *
+     * @param pattern search pattern
+     * @param data input data
+     * @param nth_difference difference
+     * @return
+     */
+    static List<String> getNthDifferenceWords(String pattern, String[] data, int nth_difference) {
+        Map<Character, Integer> frequencyTable = new HashMap<>();
+        List<String> matchedWords = new ArrayList<>();
+        //Code complexity : O(n)
+        for (int i = 0; i < pattern.length(); ++i) {
+            frequencyTable.put(pattern.charAt(i), 1);
+        }
+        //Code complexity : O(m) where m is size of entire input;
+        for (String input : data) {
+            int matchCounter = 0;
+            for (int j=0; j<input.length(); ++j){
+                if(frequencyTable.containsKey(input.charAt(j))){
+                    ++matchCounter;
+                }
+            }
+            //System.out.println("matched=" + matchCounter);
+            if(input.length() <= pattern.length() && (matchCounter == pattern.length() - nth_difference)){
+                    matchedWords.add(input);
+            }
+            if((input.length() - pattern.length() == 1) && (matchCounter >= input.length() - nth_difference)){
+                matchedWords.add(input);
+            }
+       }
+        return matchedWords;
+    }
+
+}
